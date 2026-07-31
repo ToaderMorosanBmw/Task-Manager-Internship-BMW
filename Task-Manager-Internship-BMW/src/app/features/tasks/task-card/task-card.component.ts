b455ac12c3ea1c753647f8b1b0ad477c30e6cd3f
cornelius756 <stefan.corneliu.marinescu@bmwtechworks.ro>
2026-07-31 13:41:27 +0300

    feat: enhance task management features with new components and directives
    - Added TaskDashboardComponent to manage task display.
    - Implemented category and priority color directives for visual task differentiation.
    - Enhanced task model to include color property for categories.

--- Task-Manager-Internship-BMW/src/app/features/tasks/task-card/task-card.component.ts
+++ Task-Manager-Internship-BMW/src/app/features/tasks/task-card/task-card.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Task } from '../../../core/models/task.model';
+import { CategoryColor } from '../../../shared/directives/category-color';
 
 @Component({
   selector: 'app-task-card',
   standalone: true,
-  imports: [],
+  imports: [CategoryColor],
   templateUrl: './task-card.component.html',
   styleUrl: './task-card.component.css'
 })
 export class TaskCardComponent {
-
+  @Input()
+  task!: Task;
 }
