014b7bcbfe417a093e280a968da04d69fe5d5e30
cornelius756 <stefan.corneliu.marinescu@bmwtechworks.ro>
2026-08-03 11:48:21 +0300

    feat: enhance task management with subtasks and improved task details

--- Task-Manager-Internship-BMW/src/app/core/services/category.service.ts
+++ Task-Manager-Internship-BMW/src/app/core/services/category.service.ts
@@ -15,7 +15,7 @@ export class CategoryService {
         return this.http.get<Category[]>(this.apiUrl);
     }
     
-    getCategoryById(id: number): Observable<Category> {
+    getCategoryById(id: string): Observable<Category> {
         return this.http.get<Category>(`${this.apiUrl}/${id}`);
     }   
 
@@ -23,11 +23,11 @@ export class CategoryService {
         return this.http.post<Category>(this.apiUrl, category);
     }
 
-    updateCategory(id: number, category: Category): Observable<Category> {
+    updateCategory(id: string, category: Category): Observable<Category> {
         return this.http.put<Category>(`${this.apiUrl}/${id}`, category);
     }
 
-    deleteCategory(id: number): Observable<void> {
+    deleteCategory(id: string): Observable<void> {
         return this.http.delete<void>(`${this.apiUrl}/${id}`);
     }
 }
