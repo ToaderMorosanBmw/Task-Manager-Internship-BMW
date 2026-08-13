0e624fbb90ff1240f99776e5c11ca29a0289590b
cornelius756 <stefan.corneliu.marinescu@bmwtechworks.ro>
2026-08-13 11:26:05 +0300

    Implemented Snack Notifications

--- Task-Manager-Internship-BMW/src/app/features/tasks/task-modal/task-modal.component.ts
+++ Task-Manager-Internship-BMW/src/app/features/tasks/task-modal/task-modal.component.ts
@@ -6,6 +6,7 @@ import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/materia
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Subtask } from '../../../core/models/subtask.model';
 import { TaskWithCategory } from '../../../core/models/task-with-category.model';
 
@@ -33,7 +34,8 @@ export class TaskModalComponent {
 
   constructor(
     public dialogRef: MatDialogRef<TaskModalComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: { task: TaskWithCategory }
+    @Inject(MAT_DIALOG_DATA) public data: { task: TaskWithCategory },
+    private snackBar: MatSnackBar
   ) {
     this.task = { ...data.task };
     this.task.tags = this.task.tags ?? [];
@@ -50,6 +52,13 @@ export class TaskModalComponent {
 
     if (!this.task.tags.includes(tag)) {
       this.task.tags.push(tag);
+    } else {
+      this.snackBar.open('Tag already exists!', 'Close', {
+        duration: 3000,
+        horizontalPosition: 'right',
+        verticalPosition: 'bottom',
+        panelClass: ['error-snackbar'],
+      });
     }
 
     this.newTag = '';
@@ -95,6 +104,13 @@ export class TaskModalComponent {
       this.task.dueDate = undefined;
     }
 
+    this.snackBar.open('Task saved successfully!', 'Close', {
+      duration: 3000,
+      horizontalPosition: 'right',
+      verticalPosition: 'bottom',
+      panelClass: ['success-snackbar'],
+    });
+
     this.dialogRef.close(this.task);
   }
 
@@ -121,6 +137,13 @@ export class TaskModalComponent {
       this.dialogRef.close();
       return;
     }
+
+    this.snackBar.open('Task deleted successfully!', 'Close', {
+      duration: 3000,
+      horizontalPosition: 'right',
+      verticalPosition: 'bottom',
+      panelClass: ['success-snackbar'],
+    });
 
     this.dialogRef.close({ deleted: true, id: this.task.id });
   }
